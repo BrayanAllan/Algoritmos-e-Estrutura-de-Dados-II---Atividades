663fb7c8a074db849e9ebab2db392d32ca535bfc
BrayanAllan <163424262+BrayanAllan@users.noreply.github.com>
2024-03-14 11:39:05 -0300

    Update Atividade2_Ordenação.py

--- Atividade2_Ordenação.py
+++ Atividade2_Ordenação.py
@@ -1,5 +1,17 @@
 import random
 
+def validate_ordering(ordered_list):
+
+    for i in range (len(ordered_list)-1):
+        
+        if (ordered_list[i] <= ordered_list[i+1]):
+            
+            return True
+        
+        else:
+
+            return False
+
 list_size = 10
 
 random_list = [random.randint(0,1000) for _ in range(list_size)]
@@ -9,5 +21,10 @@ print(f"Lista desordenada: {random_list}")
 print(f"\nLista ordenada: {ordered_list}")
 
 
+if validate_ordering(ordered_list) == True:
 
+    print("A lista está ordenada corretamente!")
 
+else:
+
+    print("A lista não está ordenada corretamente!")
