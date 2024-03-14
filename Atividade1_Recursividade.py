6a8fe000d7eedc3e7e31c8b11587f64b2ac576ad
BrayanAllan <163424262+BrayanAllan@users.noreply.github.com>
2024-03-14 10:33:56 -0300

    Add files via upload

---
+++ Atividade1_Recursividade.py
@@ -0,0 +1,25 @@
+def factorial(number):
+
+    if (number == 0):
+        
+        return  1
+    
+    else:
+
+      return number * factorial (number-1)
+
+number = 0
+
+while(number != int):
+
+    try:
+        number = int(input("Informe o número que deseja calcular o fatorial: "))
+        print(f"O fatorial de {number} é {(factorial(number))}.")
+        break
+
+    except:
+
+        print("Informe um número!")
+
+    
+        
